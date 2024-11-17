e822b2970ed2d679163e3945bc0d140e05f8cb1c
lazy-pr0grammer <lazyprogrammer21@gmail.com>
2024-11-17 23:58:51 +0600

    feat: hilt support has been added use `@RepoInject` to inject ApiService.kt into the generated repository

--- crepo-processor/build.gradle.kts
+++ crepo-processor/build.gradle.kts
@@ -2,11 +2,13 @@ plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
     id("com.google.devtools.ksp") version "2.0.0-1.0.24"
+    id("maven-publish")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
 }
 
 kotlin {
@@ -18,4 +20,12 @@ kotlin {
 dependencies {
     api(project(":crepo"))
     implementation(libs.symbol.processing.api)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
 }
--- crepo/build.gradle.kts
+++ crepo/build.gradle.kts
@@ -1,15 +1,25 @@
 plugins {
     id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
+    id("maven-publish")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+    withSourcesJar()
 }
 
 kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
     }
 }
